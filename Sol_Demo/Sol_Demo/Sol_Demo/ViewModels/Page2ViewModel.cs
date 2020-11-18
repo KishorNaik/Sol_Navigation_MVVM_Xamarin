f75f882fbd8415fd9dff367fa9f14ecce0590a9e
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-18 18:14:31 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Sol_Demo/ViewModels/Page2ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Sol_Demo.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Sol_Demo.ViewModels
+{
+    public class Page2ViewModel
+    {
+        private INavigation navigation = null;
+
+        public Page2ViewModel()
+        {
+            NavigatePage3 = new Command(async () => await this.NavigatePage3Async());
+            NavigateMainPage = new Command(async () => await this.NavigateMainPageAsync());
+        }
+
+        public ICommand NavigatePage3 { get; set; }
+
+        public ICommand NavigateMainPage { get; set; }
+
+        private async Task NavigatePage3Async()
+        {
+            navigation = Application.Current.MainPage.Navigation;
+            await navigation.PushAsync(new Page3());
+        }
+
+        private async Task NavigateMainPageAsync()
+        {
+            navigation = Application.Current.MainPage.Navigation;
+            await navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Sol_Demo/ViewModels/Page3ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Sol_Demo.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Sol_Demo.ViewModels
+{
+    public class Page3ViewModel
+    {
+        private INavigation navigation = null;
+
+        public Page3ViewModel()
+        {
+            NavigatePag2 = new Command(async () => await this.NavigatePage2Async());
+        }
+
+        public ICommand NavigatePag2 { get; set; }
+
+        private async Task NavigatePage2Async()
+        {
+            navigation = Application.Current.MainPage.Navigation;
+            await navigation.PushAsync(new Page2());
+        }
+    }
+}
